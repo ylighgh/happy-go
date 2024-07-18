52c295c488da4ba43fbdc0a7b718e3c5e7613265
yinlei <yinlei@axzo.cn>
2024-07-18 22:20:27 +0800

    fix

--- utils/hello.go
+++ utils/hello.go
@@ -5,6 +5,6 @@ import "fmt"
 // Hello 为指定的人返回问候语.
 func Hello(name string) string {
 	// 返回在消息中嵌入名称的问候语.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf("Hi, %v!", name)
 	return message
 }
